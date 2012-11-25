73dc6efe794fc53ea3f01a5715d9a53a15ddf607
Michael Brodhead <mkb@orthogonal.org>
2012-11-25 07:48:20 +0000

    don't clobber existing avahi imstall

--- setup/20_avahi.sh
+++ setup/20_avahi.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-emit 'Installing and configuring avahi-daemon...'
-apt-get install -y avahi-daemon
-insserv avahi-daemon
+install_avahi_daemon() {
+  emit 'Checking for avahi-daemon...'
+  dpkg -L avahi-daemonsd > /dev/null 2>&1 || echo 'Found. Skipping setup.' && return
 
-cat <<EOT >/etc/avahi/services/multiple.service
-<?xml version="1.0" standalone='no'?>
-<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-<service-group>
-  <name replace-wildcards="yes">%h</name>
-  <service>
-    <type>_device-info._tcp</type>
-    <port>0</port>
-    <txt-record>model=RackMac</txt-record>
-  </service>
-  <service>
-    <type>_ssh._tcp</type>
-    <port>22</port>
-  </service>
-</service-group>
+  local avahi_config='/etc/avahi/services/multiple.service'
+  [ -f $avahi_config ] && echo "$avahi_config already exists. Skipping avahi setup." && return
+
+  apt-get install -y avahi-daemon
+  insserv avahi-daemon
+
+  cat <<EOT > $avahi_config
+  <?xml version="1.0" standalone='no'?>
+  <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+  <service-group>
+    <name replace-wildcards="yes">%h</name>
+    <service>
+      <type>_device-info._tcp</type>
+      <port>0</port>
+      <txt-record>model=RackMac</txt-record>
+    </service>
+    <service>
+      <type>_ssh._tcp</type>
+      <port>22</port>
+    </service>
+  </service-group>
 EOT
 
-/etc/init.d/avahi-daemon restart
+  /etc/init.d/avahi-daemon restart
+}
 
+install_avahi_daemon
