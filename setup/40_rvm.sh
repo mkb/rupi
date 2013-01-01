bd80ee0d4ac8f98608f9e72cf7020a9e3a161e1b
Michael Brodhead <mkb@orthogonal.org>
2013-01-01 01:21:15 -1000

    newline strangeness

--- setup/40_rvm.sh
+++ setup/40_rvm.sh
@@ -3,6 +3,6 @@
 emit "Installing Ruby Version Manager (rvm)..."
 su - pi -c 'curl -L get.rvm.io | bash -s stable --without-gems="rvm rubygems-bundler"'
 . ~pi/.rvm/scripts/rvm
-echo "\n\n" >> .bashrc
+echo >> .bashrc
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> .bashrc
-echo "\n\n" >> .bashrc
+echo >> .bashrc
