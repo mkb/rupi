bde0706f3db522d8ffd4bb16d857211626940a53
Michael Brodhead <mkb@orthogonal.org>
2013-01-01 01:11:02 -1000

    just can't live without that bashrc change

--- setup.sh
+++ setup.sh
@@ -88,6 +88,9 @@ apt-get install -y autoconf automake libtool bison
 emit "Installing Ruby Version Manager (rvm)..."
 su - pi -c 'curl -L get.rvm.io | bash -s stable --without-gems="rvm rubygems-bundler"'
 . ~pi/.rvm/scripts/rvm
+echo "\n\n" >> .bashrc
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> .bashrc
+echo "\n\n" >> .bashrc
 
 
 #######
